c18f19dcea5a1d7d61ffca84338f549f8cfe87d5
maiconboer <maicon_boer@hotmail.com>
2020-08-23 15:45:17 -0300

    feat: challenge finished

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,74 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
-  ) {}
+  ) { }
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const checkCustomer = await this.customersRepository.findById(customer_id);
+
+    if (!checkCustomer) {
+      throw new AppError('Customer not found');
+    }
+
+    const checProducts = await this.productsRepository.findAllById(products);
+
+    if (!checProducts.length) {
+      throw new AppError('Products not found');
+    }
+
+    const productsIdsExistents = checProducts.map(product => product.id);
+
+    const checkInexistentProducts = checProducts.filter(
+      product => !productsIdsExistents.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(`Products not found ${checkInexistentProducts[0].id}`);
+    }
+
+    const findNoStockProduct = products.filter(
+      product =>
+        checProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (findNoStockProduct.length) {
+      throw new AppError(
+        `Products without stock ${findNoStockProduct[0].quantity}`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: checProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: checkCustomer,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderedProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        checProducts.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
   }
 }
 
